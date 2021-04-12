2256187262f0c38bc890af6dcd1c42fa8690ce3a
SooHwanCho <chh0107@gmail.com>
2021-04-12 14:25:23 +0900

    프로젝트 최초 설정

---
+++ initial/TDDRandomGameModel/PositiveIntegerGeneratable.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+public protocol PositiveIntegerGeneratable {
+    func generateLessThanOrEqualToHundread() -> Int
+}
---
+++ initial/TDDRandomGameModel/TDDRandomGameModel.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for TDDRandomGameModel.
+FOUNDATION_EXPORT double TDDRandomGameModelVersionNumber;
+
+//! Project version string for TDDRandomGameModel.
+FOUNDATION_EXPORT const unsigned char TDDRandomGameModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TDDRandomGameModel/PublicHeader.h>
+
+
