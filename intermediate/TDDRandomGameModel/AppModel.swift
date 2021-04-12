be89b195b053dbfd4324dce75d8a4a2f353e128d
SooHwanCho <chh0107@gmail.com>
2021-04-12 17:56:25 +0900

    3번째 테스트 성공

--- intermediate/TDDRandomGameModel/AppModel.swift
+++ intermediate/TDDRandomGameModel/AppModel.swift
@@ -5,15 +5,15 @@ public final class AppModel {
 
     }
 
-    public var isCompleted: Bool {
-        return false
-    }
+    public var isCompleted: Bool = false
 
     public func flushOutput() -> String? {
         return "1: Single player game" + "\n" + "2: Multiplayer game" + "\n" + "3: Exit" + "\n" + "Enter selection: "
     }
 
     public func processInput(_ input: String) {
-
+        if input == "3" {
+            self.isCompleted = true
+        }
     }
 }
